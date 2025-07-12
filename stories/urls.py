e472e1c78b649b4bfeb4ca9e912339ab4b9e099c
Shrichand Thapa <paulsandy321@gmail.com>
2025-07-13 00:52:45 +0530

    updated all

--- stories/urls.py
+++ stories/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path("visualstories/<str:slug>/", views.story_detail, name="story_detail"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
